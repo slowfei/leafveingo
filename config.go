aa4487acb890c569fa8d246f36f5f6a0ba634a5e
slowfei <slowfei@foxmail.com>
2013-11-07 17:45:52 +0800

    正在整理配置文件的参数

--- config.go
+++ config.go
@@ -17,5 +17,57 @@
 //  Email  slowfei@foxmail.com
 //  Home   http://www.slowfei.com
 
-//	leafveingo文件配置 TODO等待编写
+//	leafveingo文件配置加载与初始化
 package leafveingo
+
+import (
+	"fmt"
+	"github.com/slowfei/gosfcore/utils/filemanager"
+	"path/filepath"
+)
+
+//	Leafveingo config
+type Config struct {
+	appName             string `json:"AppName"`
+	appVersion          string `json:"AppVersion"`
+	suffixs             string `json:"Suffixs"`
+	staticFileSuffixs   string `json:"StaticFileSuffixs"`
+	charset             string
+	isRespWriteCompress bool
+	fileUploadSize      int64
+	sessionMaxlifeTime  int32
+	serverTimeout       int64
+	port                int
+	addr                string
+	webRootDir          string
+	templateDir         string
+	templateSuffix      string
+	isUseSession        bool
+	isGCSession         bool
+	logConfig           string
+}
+
+//
+//	启动程序前的初始化Leafvein，这个是根据配置文件进行加载的
+//
+//	@configPath 绝对或相对路径，相对路径从执行文件目录开始查找
+//	@return Leafvein的初始化对象，根据配置文件进行配置信息的。
+func InitLeafvein(configPath string) ISFLeafvein {
+	if 0 == len(configPath) {
+		fmt.Println("InitLeafvein config path nil:%v", configPath)
+		return nil
+	}
+	if nil != _thisLeafvein {
+		fmt.Println("Leafveingo Has been initialized.")
+		return _thisLeafvein
+	}
+
+	var path string
+
+	if filepath.IsAbs(configPath) {
+		path = configPath
+	} else {
+		path = SFFileManager.GetExceDir() + configPath
+	}
+
+}
