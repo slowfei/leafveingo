180040ffac0e8417d1f8802023e772015da013ff
slowfei <slowfei@foxmail.com>
2014-06-13 02:45:48 +0800

    重新整改架构

--- config.go
+++ config.go
@@ -13,12 +13,12 @@
 //   limitations under the License.
 //
 //  Create on 2013-11-06
-//  Update on 2013-11-06
-//  Email  slowfei@foxmail.com
+//  Update on 2014-06-12
+//  Email  slowfei#foxmail.com
 //  Home   http://www.slowfei.com
 
-//	leafveingo文件配置加载与初始化
 //
+//	leafveingo config handle
 //
 package leafveingo
 
@@ -32,18 +32,15 @@ import (
 )
 
 const (
-	//  default port 8080
-	DEFAULT_PORT int = 8080
-	//	default http addr
-	DEFAULT_HTTP_ADDR = "127.0.0.1"
+	//	default web root dir name
+	DEFAULT_WEBROOT_DIR_NAME = "webRoot"
+	//	default template dir name
+	DEFAULT_TEMPLATE_DIR_NAME = "template"
+
 	//	file size default upload  32M
 	DEFAULT_FILE_UPLOAD_SIZE int64 = 32 << 20
 	//	server time out default 0
 	DEFAULT_SERVER_TIMEOUT = 0
-	//	default web root dir
-	DEFAULT_WEBROOT_DIR = "webRoot"
-	//	default template dir
-	DEFAULT_TEMPLATE_DIR = "template"
 	//	default template suffix
 	DEFAULT_TEMPLATE_SUFFIX = ".tpl"
 	//	default html charset
@@ -63,218 +60,95 @@ const (
 var (
 	_defaultConfigJson = `
 	{
-		"Port"			:8080,
-		"Addr"			:"127.0.0.1",
-		"ServerTimeout"		:0,
-		"AppName"		:"LeafveingoWeb",
 		"AppVersion"		:"1.0",
+		"FileUploadSize"	:33554432,
+		"Charset"		:"utf-8",
 		"Suffixs"		:[""],
 		"StaticFileSuffixs"	:[".js", ".css", ".png", ".jpg", ".gif", ".ico", ".html"],
-		"Charset"		:"utf-8",
-		"IsRespWriteCompress"	:true,
-		"IsCompactHTML"			:true,
-		"FileUploadSize"	:33554432,
-		"IsUseSession"		:true,
-		"IsGCSession"		:true,
+		"ServerTimeout"		:0,
 		"SessionMaxlifeTime"	:1800,
-		"WebRootDir"		:"webRoot",
-		"TemplateDir"		:"template",
+
 		"TemplateSuffix"	:".tpl",
+		"IsCompactHTML"			:true,
+
 		"LogConfigPath"		:"config/log.conf",
 		"LogChannelSize"	:5000,
+
+		"IsRespWriteCompress"	:true,
+		
 		"UserData"		:{}
 	}`
 )
 
-//	Leafveingo config
+/**
+ *	leafveingo config
+ *	default see _defaultConfigJson
+ */
 type Config struct {
-	Port                int               // default port 8080
-	Addr                string            // default http addr 127.0.0.1
-	ServerTimeout       int64             // server time out default 0
-	AppName             string            // app name default "LeafveingoWeb"
-	AppVersion          string            // app version default 1.0
-	Suffixs             []string          // http request url suffixs default ""
-	StaticFileSuffixs   []string          // static file suffixs default ".js", ".css", ".png", ".jpg", ".gif", ".ico", ".html"
-	Charset             string            // default html charset utf-8
-	IsRespWriteCompress bool              // default response write compress true
-	IsCompactHTML       bool              // out html compact remove (\t \n space) sign,only template file out use. default true
-	FileUploadSize      int64             // file size default upload  32M
-	IsUseSession        bool              // default use http session true
-	IsGCSession         bool              // default gc http session true
-	SessionMaxlifeTime  int32             // default session max life time 1800 seconds
-	WebRootDir          string            // default web root dir "webRoot"
-	TemplateDir         string            // default template dir "template"
-	TemplateSuffix      string            // default template suffix ".tpl"
-	LogConfigPath       string            // default log config path relative path"config/app.conf"
-	LogChannelSize      int               // default log channel size 5000
-	UserData            map[string]string //
+	AppVersion         string   // app version.
+	FileUploadSize     int64    // file size upload
+	Charset            string   // html encode type
+	Suffixs            []string // http url suffixs
+	StaticFileSuffixs  []string // supported static file suffixs
+	ServerTimeout      int64    // server time out, default 0
+	SessionMaxlifeTime int32    // http session maxlife time, unit second. use session set
+
+	TemplateSuffix string // template suffix
+	IsCompactHTML  bool   // is Compact HTML, 默认true
+
+	LogConfigPath  string // log config path
+	LogChannelSize int    // log channel size default 5000
+
+	// is ResponseWriter writer compress gizp...
+	// According Accept-Encoding select compress type
+	// default true
+	IsRespWriteCompress bool
+
+	UserData map[string]string // user custom config other info
 }
 
-func (c *Config) Get(key string) string {
-	return c.UserData[key]
-}
+/**
+ *	conifg load
+ *
+ *	@param jsonData
+ *	@return load error info
+ */
+func configLoadByJson(jsonData []byte) (Config, error) {
 
-//	load config
-//	@configPath
-func loadConfig(configPath string) error {
-	if nil == _thisLeafvein {
-		return ErrLeafveingoNotInit
-	}
-	if nil == _thisLeafvein.Config() {
-		return ErrLeafveingoConfigNotInit
-	}
+	c := new(Config)
 
-	if 0 != len(configPath) {
-		// _rwmutex.Lock()
-		// defer _rwmutex.Unlock()
-
-		var path string
-		if filepath.IsAbs(configPath) {
-			path = configPath
-		} else {
-			path = filepath.Join(SFFileManager.GetExecDir(), configPath)
-		}
-
-		isExists, isDir, _ := SFFileManager.Exists(path)
-		if !isExists || isDir {
-			return errors.New("failed to load configuration file:" + configPath)
-		}
-
-		jsonData, e1 := ioutil.ReadFile(path)
-		if nil != e1 {
-			return e1
-		}
-
-		e2 := json.Unmarshal(jsonData, _thisLeafvein.Config())
-
-		if nil != e2 {
-			return e2
-		}
-	}
-	setLeafveingoConfig()
-	return nil
-}
-
-//	load config
-//	@jsonData
-func loadConfigByJson(jsonData []byte) error {
-	if nil == _thisLeafvein {
-		return ErrLeafveingoNotInit
-	}
-
-	if nil == _thisLeafvein.Config() {
-		return ErrLeafveingoConfigNotInit
-	}
-
-	e2 := json.Unmarshal(jsonData, _thisLeafvein.Config())
+	e2 := json.Unmarshal(jsonData, c)
 	if nil != e2 {
-		return e2
+		return _, e2
 	}
-	setLeafveingoConfig()
-	return nil
+
+	return c, nil
 }
 
-//
-//	启动程序前的初始化Leafvein，这个是根据配置文件进行加载的
-//
-//	@configPath 绝对或相对路径，相对路径从执行文件目录开始查找
-//	@return Leafvein的初始化对象，根据配置文件进行配置信息的。
-func InitLeafvein(configPath string) ISFLeafvein {
-	if nil != _thisLeafvein {
-		panic(ErrLeafveingoHasbeenInit)
-	}
+/**
+ *	conifg load
+ *
+ *	@param configPath
+ *	@return error info
+ */
+func configLoadByFilepath(configPath string) (Config, error) {
 
-	var privatelv sfLeafvein = sfLeafvein{}
-	_thisLeafvein = &privatelv
-	privatelv.initPrivate()
-
-	if 0 != len(configPath) {
-		err := loadConfig(configPath)
-		if nil != err {
-			panic(NewLeafveingoError(err.Error()))
-		}
+	var path string
+	if filepath.IsAbs(configPath) {
+		path = configPath
 	} else {
-		fmt.Println("InitLeafvein Failed to load config file: ", configPath)
-		fmt.Println("Use default config info:\n", _defaultConfigJson)
+		path = filepath.Join(SFFileManager.GetExecDir(), configPath)
 	}
 
-	return _thisLeafvein
-}
-
-//	进行配置的设置
-func setLeafveingoConfig() {
-	if nil != _thisLeafvein {
-		cf := _thisLeafvein.Config()
-		if nil != cf {
-			if !_thisLeafvein.IsStart() {
-				//	启动后不能修改的配置信息
-
-				// appName string
-				_thisLeafvein.SetAppName(cf.AppName)
-
-				// server time out
-				_thisLeafvein.SetServerTimeout(cf.ServerTimeout)
-
-				// default 8080
-				_thisLeafvein.SetPort(cf.Port)
-
-				// http addr default 127.0.0.1
-				// addr string
-				_thisLeafvein.SetAddr(cf.Addr)
-
-				// web directory, can read and write to the directory
-				// primary storage html, css, js, image, zip the file
-				// webRootDir string
-				_thisLeafvein.SetWebRootDir(cf.WebRootDir)
-
-				// template dir, storage template file
-				// templateDir string
-				_thisLeafvein.SerTemplateDir(cf.TemplateDir)
-
-				// template suffix, default ".tpl"
-				// templateSuffix string
-				_thisLeafvein.SetTemplateSuffix(cf.TemplateSuffix)
-
-				// isUseSession bool // is use session
-				_thisLeafvein.SetUseSession(cf.IsUseSession)
-
-				// isGCSession  bool // is auto GC session
-				_thisLeafvein.SetGCSession(cf.IsGCSession)
-
-				// log config path
-				// logConfPath string
-				_thisLeafvein.SetLogConfPath(cf.LogConfigPath)
-
-				// log channel size default 5000
-				// logChannelSize int
-				_thisLeafvein.SetLogChannelSize(cf.LogChannelSize)
-
-			}
-
-			// 	appVersion string
-			_thisLeafvein.SetAppVersion(cf.AppVersion)
-
-			// http url suffixs
-			_thisLeafvein.SetHTTPSuffixs(cf.Suffixs...)
-
-			// supported static file suffixs
-			_thisLeafvein.SetStaticFileSuffixs(cf.StaticFileSuffixs...)
-
-			// html encode type, charset
-			_thisLeafvein.SetCharset(cf.Charset)
-
-			// isRespWriteCompress bool
-			_thisLeafvein.SetRespWriteCompress(cf.IsRespWriteCompress)
-
-			//	IsCompactHTML bool
-			_thisLeafvein.SetCompactHTML(cf.IsCompactHTML)
-
-			// file size upload 32M fileUploadSize
-			_thisLeafvein.SetFileUploadSize(cf.FileUploadSize)
-
-			// sessionMaxlifeTime int32
-			_thisLeafvein.SetSessionMaxlifeTime(cf.SessionMaxlifeTime)
-
-		}
+	isExists, isDir, _ := SFFileManager.Exists(path)
+	if !isExists || isDir {
+		return _, errors.New("failed to load configuration file:" + configPath)
 	}
+
+	jsonData, e1 := ioutil.ReadFile(path)
+	if nil != e1 {
+		return _, e1
+	}
+
+	return configLoadByJson(jsonData)
 }
